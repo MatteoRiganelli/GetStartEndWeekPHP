3cfa7bb36ccb4b4165b759674ed1168791d73a55
Matteo Riganelli <mriganelli@engagepeople.com>
2018-07-31 10:44:47 +0200

    function that returns the beginning and end of each week for a specific month

---
+++ getDayWeek.php
@@ -0,0 +1,52 @@
+<?php
+
+        public function getDateDayWeek ($month){
+
+        $j=1;
+        $k=1;
+
+        $month = 2;
+
+        $year = Carbon::now()->year;
+        $date = Carbon::createFromDate($year,$month, 1);
+
+        $start = [];
+        $end = [];
+
+        if(Carbon::createFromDate($year, $month, 1)->isSunday()){
+            $start[$j] = Carbon::createFromDate($year, $month, 1)->firstOfMonth()->day;
+            $j++;
+            $end[$k] = Carbon::createFromDate($year, $month, 1)->firstOfMonth()->day;
+            $k++;
+        }
+        else{
+            $start[$j] = Carbon::createFromDate($year, $month, 1)->firstOfMonth()->day;
+            $j++;
+        }
+
+        for ($i=1; $i <= $date->daysInMonth ; $i++) {
+
+            $isMonday = Carbon::createFromDate($year, $month, $i)->isMonday();
+            $isSunday = Carbon::createFromDate($year, $month, $i)->isSunday();
+
+            if( $isMonday == true ) {
+                $start[$j] = $i;
+                $j++;
+            }
+            elseif( $isSunday == true && $i != 1){
+                $end[$k]= $i;
+                $k++;
+            }
+
+        }
+
+        $end[$k] = Carbon::createFromDate($year, $month, 1)->endOfMonth()->day;
+
+        $object = (object) ['start' => $start,
+                            'end' => $end];
+
+        return $object;
+
+    }
+
+?>
